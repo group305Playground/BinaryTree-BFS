0aa6f1943bd07d588b7777d6a54caca907df2622
marcus02 <marcus02@opensource.gov>
2018-08-16 11:48:26 -0400

    Comment out Hello

--- BinaryTree-BFS/src/main.java
+++ BinaryTree-BFS/src/main.java
@@ -13,7 +13,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		// System.out.println("Hello");
 		
 		Node root = new Node(5);
 		root.setLeft(new Node(10));
